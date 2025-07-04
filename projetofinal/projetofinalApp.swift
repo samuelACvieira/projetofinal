47044a718c1bb8bc7a15c17d3c759d4120eb2a98
Samuel Arthur Cruz Vieira <samuel.vieira@aluno.ifce.edu.br>
2025-07-04 16:46:27 -0300

    Initial Commit

---
+++ projetofinal/projetofinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  projetofinalApp.swift
+//  projetofinal
+//
+//  Created by Foundation on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct projetofinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ projetofinalTests/projetofinalTests.swift
@@ -0,0 +1,17 @@
+//
+//  projetofinalTests.swift
+//  projetofinalTests
+//
+//  Created by Foundation on 04/07/25.
+//
+
+import Testing
+@testable import projetofinal
+
+struct projetofinalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
