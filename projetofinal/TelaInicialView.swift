3f4eedaf92d2c3efbe56e262e2bdb9d6d81548d1
Samuel Arthur Cruz Vieira <samuel.vieira@aluno.ifce.edu.br>
2025-07-22 15:00:26 -0300

    comitando alteracoes da tela inicial + colordex

--- projetofinal/TelaInicialView.swift
+++ projetofinal/TelaInicialView.swift
@@ -1,24 +1,58 @@
-//
-//  ContentView.swift
-//  projetofinal
-//
-//  Created by Foundation on 04/07/25.
-//
-
 import SwiftUI
-
+import UIKit
 struct TelaInicialView: View {
+    @State private var valorAtual: Double = 400
+    let meta: Double = 1000
+    @State private var valorParaAdicionar = ""
     var body: some View {
-        VStack {
-            Image(systemName:"globe")
-                .imageScale(.large)
-                .foregroundStyle(.green)
-            Text("saldo")
-        }
+        VStack(spacing: 5) {
+            
+            //textao grande
+            Text("Saldo")
+                .font(.title)
+                .foregroundColor(Color(hex: "5EB169")   )
+            
+            
+            // Barra de progresso da meta
+            ProgressView(value: valorAtual, total: meta)
+                .progressViewStyle(LinearProgressViewStyle(tint: Color(hex: "5EB169")))
+                .padding(.horizontal, 0)
+                .scaleEffect(x: 1, y: 3)
+                
+            // texto abaixo da barra de progresso
+            Text("R$ \(String(format: "%.2f", valorAtual)) de R$ \(String(format: "%.2f", meta))")
+                .font(.subheadline)
+                .foregroundColor(Color(hex: "5EB169"))
+            
+            TextField("digite o valor", text: $valorParaAdicionar)
+                .keyboardType(.numberPad)
+                .cornerRadius(0)
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding()
+            //gastos totais
+            RoundedRectangle(cornerRadius: 12)
+                .frame(width:174, height:76)
+                .position(x: 100 , y:4)
+                .foregroundStyle(Color(hex: "e65252"))
+            // botao para adicionar saldo
+            Button("Adicionar saldo") {
+                if let valorDigitado = Double(valorParaAdicionar.replacingOccurrences(of: ",", with: ".")) {
+                    if valorDigitado > 0 && valorAtual + valorDigitado <= meta {
+                        valorAtual += valorDigitado
+                        valorParaAdicionar = ""
+                    }
+                }
+            
+            }
+        } //configuraçoes do botao
         .padding()
+        .background(Color.white)
+        .foregroundStyle(Color(hex: "5eb169"))
+        .cornerRadius(40)
     }
 }
 
+// ✅ Preview fora da struct principal
 #Preview {
     TelaInicialView()
 }
---
+++ projetofinal/color+dex.swift
@@ -0,0 +1,25 @@
+
+//
+//  color+dex.swift
+//  projetofinal
+//
+//  Created by Foundation on 18/07/25.
+//
+// EXTENSAO COMPLETAMENTE DESNCESSECARIA SO PARA EU PODER ME GUIAR MELHOR FAZENDO O FRONT
+// CORES EM HEXADECIMAL
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.replacingOccurrences(of: "#", with: "")
+        let scanner = Scanner(string: hex)
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >> 8) & 0xFF) / 255.0
+        let b = Double(rgb & 0xFF) / 255.0
+
+        self.init(red: r, green: g, blue: b)
+    }
+}
