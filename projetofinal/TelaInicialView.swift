179491942b070048265319257f7ae49d3503f857
Samuel Arthur Cruz Vieira <samuel.vieira@aluno.ifce.edu.br>
2025-07-22 16:00:08 -0300

    mudanças na tela incial

--- projetofinal/TelaInicialView.swift
+++ projetofinal/TelaInicialView.swift
@@ -8,47 +8,68 @@ struct TelaInicialView: View {
         VStack(spacing: 5) {
             
             //textao grande
-            Text("Saldo")
-                .font(.title)
-                .foregroundColor(Color(hex: "5EB169")   )
-            
+//            Text("Saldo")
+//                .font(.title)
+//                .foregroundColor(Color(hex: "5EB169")   )
+//            
             
             // Barra de progresso da meta
-            ProgressView(value: valorAtual, total: meta)
-                .progressViewStyle(LinearProgressViewStyle(tint: Color(hex: "5EB169")))
-                .padding(.horizontal, 0)
-                .scaleEffect(x: 1, y: 3)
+//            ProgressView(value: valorAtual, total: meta)
+//                .progressViewStyle(LinearProgressViewStyle(tint: Color(hex: "5EB169")))
+//                .padding(.horizontal, 0)
+//                .scaleEffect(x: 1, y: 3)
                 
             // texto abaixo da barra de progresso
-            Text("R$ \(String(format: "%.2f", valorAtual)) de R$ \(String(format: "%.2f", meta))")
-                .font(.subheadline)
-                .foregroundColor(Color(hex: "5EB169"))
+//            Text("R$ \(String(format: "%.2f", valorAtual)) de R$ \(String(format: "%.2f", meta))")
+//                .font(.subheadline)
+//                .foregroundColor(Color(hex: "5EB169"))
             
-            TextField("digite o valor", text: $valorParaAdicionar)
-                .keyboardType(.numberPad)
-                .cornerRadius(0)
-                .textFieldStyle(PlainTextFieldStyle())
-                .padding()
+//            TextField("digite o valor", text: $valorParaAdicionar)
+//                .keyboardType(.numberPad)
+//                .cornerRadius(0)
+//                .textFieldStyle(PlainTextFieldStyle())
+//                .padding()
             //gastos totais
-            RoundedRectangle(cornerRadius: 12)
-                .frame(width:174, height:76)
-                .position(x: 100 , y:4)
-                .foregroundStyle(Color(hex: "e65252"))
-            // botao para adicionar saldo
-            Button("Adicionar saldo") {
-                if let valorDigitado = Double(valorParaAdicionar.replacingOccurrences(of: ",", with: ".")) {
-                    if valorDigitado > 0 && valorAtual + valorDigitado <= meta {
-                        valorAtual += valorDigitado
-                        valorParaAdicionar = ""
-                    }
+            ZStack {
+                Color(hex: "5EB169").ignoresSafeArea()
+                
+                VStack {
+                    Text("Saldo")
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(Color(hex: "e8e8e8"))
+                        .position(x:50, y: 30)
+                    
                 }
-            
             }
+            .frame(height: 100)
+            
+            HStack(spacing: 20) {
+                RoundedRectangle(cornerRadius: 12)
+                    .frame(width:174, height:76)
+                    .foregroundStyle(Color(hex: "e65252"))
+                // ECONOMIAS TOTAIS
+                RoundedRectangle(cornerRadius: 12)
+                    .frame(width:174, height:76)
+                    .foregroundStyle(Color(hex: "e65252"))
+            }
+            .padding(.top, 12)
+            Spacer()
+            // botao para adicionar saldo
+//            Button("Adicionar saldo") {
+//                if let valorDigitado = Double(valorParaAdicionar.replacingOccurrences(of: ",", with: ".")) {
+//                    if valorDigitado > 0 && valorAtual + valorDigitado <= meta {
+//                        valorAtual += valorDigitado
+//                        valorParaAdicionar = ""
+//                    }
+//                }
+//            
+//            }
         } //configuraçoes do botao
-        .padding()
-        .background(Color.white)
-        .foregroundStyle(Color(hex: "5eb169"))
-        .cornerRadius(40)
+//        .padding()
+//        .background(Color.white)
+//        .foregroundStyle(Color(hex: "5eb169"))
+//        .cornerRadius(40)
     }
 }
 
@@ -56,3 +77,4 @@ struct TelaInicialView: View {
 #Preview {
     TelaInicialView()
 }
+
